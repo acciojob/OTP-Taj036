d257b326f0a29139bd211c6d80edee3d9046ce57
Taj036 <121137573+Taj036@users.noreply.github.com>
2023-12-29 22:32:29 +0000

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+ 
+    function handleInput(input) {
+      const codeContainer = document.getElementById("codeContainer");
+      const inputs = codeContainer.querySelectorAll(".code");
+
+      if (input.value !== "") {
+        // Move to the next input field
+        const nextInput = input.nextElementSibling;
+        if (nextInput) {
+          nextInput.focus();
+        }
+      } else {
+        // Backspace pressed, move to the previous input field
+        const prevInput = input.previousElementSibling;
+        if (prevInput) {
+          prevInput.focus();
+        }
+      }
+    }
